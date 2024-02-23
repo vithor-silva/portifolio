a5c39f4c3e5327ab13c9408c58c1dd89b85533ff
Vithor Silva <flatztr3m@gmail.com>
2024-02-23 15:55:42 -0300

    styles

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const menuIcon = document.querySelector('.menu-icon')
-  const menu = document.querySelector('.menu')
+  const menuIcon = document.querySelector('.menu-icon');
+  const menu = document.querySelector('.menu');
 
   menuIcon.addEventListener('click', function () {
-    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex'
-  })
-})
-
-
+    if (menu.style.display === 'flex') {
+      menu.style.display = 'none';
+      menuIcon.innerHTML = '&#9776;';
+    } else {
+      menu.style.display = 'flex';
+      menuIcon.innerHTML = '&#10006;';
+    }
+  });
+});
